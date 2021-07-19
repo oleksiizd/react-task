ed175f2f727e7714fcda337ec6e0abf172256d37
oleksiizd <oleksii.zd@gmail.com>
2021-07-19 17:33:39 +0300

    React Visualiser temp setup

--- src/components/TableWeather/TableWeather.tsx
+++ src/components/TableWeather/TableWeather.tsx
@@ -6,6 +6,7 @@ import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import { List, AutoSizer } from "react-virtualized";
 
 import { useSelector } from "react-redux";
 import { ParsedDataObj } from "../../redux/parsedDataSlice/types";
@@ -14,33 +15,54 @@ import { getParsedData } from "../../redux/selectors/getStoredData";
 function SimpleTable() {
   const [parsedData, parsedHeader] = useSelector(getParsedData);
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {parsedHeader.map((row: string, index: React.Key) => (
-              <TableCell size="small" align="center" key={index}>
-                {row}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {parsedData.map((row: ParsedDataObj, index: React.Key) => (
-            <TableRow key={index}>
-              <TableCell align="center">{row.yyyy}</TableCell>
-              <TableCell align="center">{row.mm}</TableCell>
-              <TableCell align="center">{row.tmax}</TableCell>
-              <TableCell align="center">{row.tmin}</TableCell>
-              <TableCell align="center">{row.af}</TableCell>
-              <TableCell align="center">{row.rain}</TableCell>
-              <TableCell align="center">{row.sun}</TableCell>
-              <TableCell align="center">{row.prov}</TableCell>
+    <>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              {parsedHeader.map((row: string, index: React.Key) => (
+                <TableCell size="small" align="center" key={index}>
+                  {row}
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+        </Table>
+      </TableContainer>
+      <TableContainer
+        component={Paper}
+        style={{
+          width: "100%",
+          height: "50vh",
+        }}
+      >
+        <AutoSizer>
+          {({ width, height }) => (
+            <List
+              width={width}
+              height={height}
+              rowHeight={50}
+              rowCount={parsedData.length}
+              rowRenderer={({ index, style, parent }) => {
+                let row = parsedData[index];
+                return (
+                  <TableRow key={index} style={style}>
+                    <TableCell align="center">{row.yyyy}</TableCell>
+                    <TableCell align="center">{row.mm}</TableCell>
+                    <TableCell align="center">{row.tmax}</TableCell>
+                    <TableCell align="center">{row.tmin}</TableCell>
+                    <TableCell align="center">{row.af}</TableCell>
+                    <TableCell align="center">{row.rain}</TableCell>
+                    <TableCell align="center">{row.sun}</TableCell>
+                    <TableCell align="center">{row.prov}</TableCell>
+                  </TableRow>
+                );
+              }}
+            />
+          )}
+        </AutoSizer>
+      </TableContainer>
+    </>
   );
 }
 
